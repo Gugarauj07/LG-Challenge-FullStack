15cf60293bacf5b5703a4ef9a2aa25fff6ffe6a8
gugarauj07 <gugaraujo01@gmail.com>
2025-03-13 11:11:47 -0400

    fix: docker compose

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000/api/v1' // Deve ser alterado para a URL de produção
+  apiUrl: '/api/v1' // Usando o proxy configurado no nginx
 };
